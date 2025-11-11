aba0e84b50e6a5580db146768e60b281e92c553a
carlinho1680 <cg.fuentes@duocuc.cl>
2025-11-11 11:14:27 -0300

    Add files via upload

---
+++ src/pages/FinalizarCompra.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const finalizarCompra = () => {
+  return (
+    <div>
+      <h1>finalizar Compra</h1>
+      <p>Formulario para finalizar la compra.</p>
+    </div>
+  );
+};
+
+export default finalizarCompra;
